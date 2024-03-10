1805f2381b64d43b1d0a0049aadb66b9ffcedd2d
FilipMolnar <F.M.T.Molnar@student.tudelft.nl>
2024-03-10 19:34:13 +0100

    new plot.py

--- plot.py
+++ plot.py
@@ -4,7 +4,7 @@ import csv
 # Example data
 
 humans={}
-
+name = 'eve'
 with open('./logs/logWillCom.csv', mode='r') as f:
     reader = csv.reader(f) 
     for r in reader:
@@ -19,18 +19,18 @@ with open('./logs/logWillCom.csv', mode='r') as f:
             humans[row[0]]['competence'] = [round(float(row[1]),2)]
             humans[row[0]]['willingness'] = [round(float(row[2]),2)]
 
-x_values = list(range(0, len(humans['alice']['willingness'])))
-y_values = humans['alice']['willingness']
+x_values = list(range(0, len(humans[name]['willingness'])))
+y_values = humans[name]['willingness']
 
 # Plotting the values
 
-plt.plot(list(range(0, len(humans['alice']['willingness']))), humans['alice']['willingness'], label='willingness')
-plt.plot(list(range(0, len(humans['alice']['competence']))), humans['alice']['competence'], label='competence')
+plt.plot(list(range(0, len(humans[name]['willingness']))), humans[name]['willingness'], label='willingness')
+plt.plot(list(range(0, len(humans[name]['competence']))), humans[name]['competence'], label='competence')
 
 # Adding labels and title
 plt.xlabel('rounds')
 plt.ylabel('score')
-plt.title('Alice')
+plt.title(name.capitalize())
 
 # Displaying legend (optional)
 plt.legend()
